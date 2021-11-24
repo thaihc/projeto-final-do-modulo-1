60fb6f3a05db477f5085c43949ef5a2a3cdb559d
thaihc <thaisherculanodecarvalho@gmail.com>
2021-11-24 12:40:08 -0300

    pags-de-gameover-adicionadas-no-jogo-do-fantasma

---
+++ espantalho/espantalho2.js
@@ -0,0 +1,19 @@
+function responder() {
+  var resposta = prompt(
+    "Marque a resposta correta: \n\n 1- Caminho Amarelo \n 2- Caminho Azul"
+  );
+  while (true) {
+    if (resposta == 1) {
+      window.location.href = "game-over-2.html";
+      break;
+    } else if (resposta == 2) {
+      window.location.href = "espantalho-fase-3.html";
+      break;
+    } else {
+      alert("Você precisa digitar 1 ou 2");
+      resposta = prompt(
+        "Marque a resposta correta \n\n 1- Caminho Amarelo \n 2- Caminho Azul"
+      );
+    }
+  }
+}
